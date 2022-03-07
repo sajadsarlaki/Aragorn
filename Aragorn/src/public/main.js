69a046c61c63bd00d29e36f6a721dae75d81bfa3
sajad <sajadsarlaki2017@gmail.com>
2022-03-07 15:50:55 +0330

    some changes happend!

--- Aragorn/src/public/main.js
+++ Aragorn/src/public/main.js
@@ -1,3 +1,2 @@
 // $ is the library reference
 
-Aragorn.Gsap.to(".box", {rotation: 27, x: 100, duration: 1});
--- Aragorn/src/server.js
+++ Aragorn/src/server.js
@@ -3,7 +3,6 @@ const path = require("path");
 const app = express()
 const port = 4000
 
-
 app.use(express.static( path.resolve(__dirname, 'public')))
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
